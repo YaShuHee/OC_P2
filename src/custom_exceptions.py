501d5a607dac29238320f837eee9bcb2aaa593c8
Timothé Gélibert <tim.gelibert@protonmail.com>
2021-03-01 12:54:47 +0100

    README.md moved to project root.

---
+++ src/custom_exceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+
+class HttpResponseError(Exception):
+    """
+    Base exception used to build common bad HTTP responses exceptions.
+    """
+    def __init__
+
+
+class Error404(Error):
