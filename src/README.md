a30240254e5ddbbd24ccb2c8c5ab434e9454f282
Timothé Gélibert <tim.gelibert@protonmail.com>
2021-03-01 12:31:33 +0100

    Future functions and comments were added.

---
+++ src/README.md
@@ -0,0 +1,10 @@
+#\[EN\] [Books to scrap](http://books.toscrape.com) scraping project for the "Books Online" library.
+##The work is still in progress.
+The actual code is more a proof of concept than the production of the final code.
+It's also a pretext to train with Git.
+
+
+#\[FR\] Projet de scraping du site [Books to scrap](http://books.toscrape.com) pour la librairie "Books Online".
+##Le travail est toujours en cours.
+Le code actuel est plus une démonstration de faisabilité qu'une réelle production du code qui sera livré.
+C'est aussi un prétexte pour m'entraîner à utiliser Git.
--- src/main.py
+++ src/main.py
@@ -2,37 +2,79 @@
 # coding: utf-8
 
 
+from functools import cache
 import requests
 from bs4 import BeautifulSoup
 
 
 def get_soup(url):
+    """
+    Returns the html code extracted from the url or an empty string.
+    TODO: Recode with exception handling?
+    """
     response = requests.get(url)
     if response.ok:
-        return BeautifulSoup(response.text)
+        return BeautifulSoup(response.text, features="html.parser")
     else:
         return ""
 
 
+@cache
 def get_links(soup):
+    """
+    Returns all the <a> elements from a soup.
+    Result is calculated only once for each 'soup' argument given thanks to @cache decorator.
+    """
     return soup.findAll("a")
 
 
 def get_categories(soup):
+    """
+    Returns a dict containing categories of books as keys and their relative url as values.
+    """
     links = get_links(soup)
-    return [
-        link['href'].removeprefix("catalogue/category/books/").removesuffix("/index.html")
+    return {
+        link.contents[0].strip(): "https://books.toscrape.com" + link['href'].replace("index.html", "")
         for link in links if "category/books/" in link["href"]
-    ]
+    }
+
+
+def get_book_info(url):
+    """
+    Returns all info from a book, using its url.
+    TODO: Coding it.
+    """
+    return {}
+
+
+def get_books_url_from_page(soup):
+    """
+    Returns a list of all the books urls from one page.
+    TODO: Coding it.
+    """
+    return ""
+
+
+def get_books_from_category(category_url):
+    """
+    Returns a list of all the books and their infos from one category, using its url.
+    """
+    pages = 1
+    while True:
+        break
 
 
 def main():
+    """TODO: """
     soup = get_soup("https://books.toscrape.com")
     categories = get_categories(soup)
     counter = 1
-    for category in categories:
-        print(f"{counter}. {category}")
+    books = {}
+    for category_name, category_url in categories.items():
+        print(f"{counter}. {category_name}: {category_url}")
         counter += 1
+        # TODO: extract books info from each category
+        # books[category_name] = get_books_from_category(category_url)
 
 
 if __name__ == '__main__':
